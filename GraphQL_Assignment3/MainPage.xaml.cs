6529e22c747906a261b6cd9f1729944a98342746
Sukanya <sukanya.c.mohan@gmail.com>
2019-08-11 11:14:56 -0400

    Initial check-in of module GraphQL_Assignment3

---
+++ GraphQL_Assignment3/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+using GraphQL_Assignment3.Model;
+using Xamarin.Forms;
+
+namespace GraphQL_Assignment3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            var client = new GraphQLClient(" https://swapi.apis.guru/");
+
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms { films {id, title, director } } }"
+            };
+            GraphQLResponse graphQLResponse = await client.PostAsync(graphQLRequest);
+            filmList.ItemsSource = graphQLResponse.Data.allFilms.films.ToObject<List<Films>>();
+
+
+        }
+    }
+}
---
+++ GraphQL_Assignment3/Model/Films.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace GraphQL_Assignment3.Model
+{
+    public class Films
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+    }
+}
